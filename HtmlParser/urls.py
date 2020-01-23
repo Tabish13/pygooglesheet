6f5ff456f4d8ea56414daa16997c869ca0865164
Tabish@132 <tabish@haptik.ai>
2020-01-23 18:29:17 +0530

    Added godrej sheet

--- HtmlParser/urls.py
+++ HtmlParser/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('htmlparser', views.HtmlParse, name='htmlparser'),
     path('incorsheet', views.IncorGsheetParser, name='incorsheet'),
+    path('godrejsheet', views.GodrejPropertiesGsheetParser, name='godrejsheet'),
 ]
